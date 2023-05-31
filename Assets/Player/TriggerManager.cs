420c7978f941ac398b5be2f0d0152e103f70c14b
Shachar Oz <shachar.oz@gmail.com>
2023-05-31 22:31:14 +0200

    initial commit

---
+++ Assets/Player/TriggerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour 
+{
+    //to complete game, reach the exit 
+    public string ExitTargetTagName = "Exit";
+    public UnityEngine.Events.UnityEvent OnMazeEnds;
+   
+    //touching lava kills the player
+    public string FailTargetTagName = "Lava";
+    public UnityEngine.Events.UnityEvent OnMazeEndFail;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with something");
+
+        if (other.tag == FailTargetTagName)
+        {
+            Debug.Log("you touched lava. you are dead");
+            //this.GetComponent<Animator>().SetTrigger("DieNow");
+            OnMazeEndFail.Invoke();
+        }
+
+        if (other.tag == ExitTargetTagName)
+        {
+            Debug.Log("well done. you exited the maze");
+            OnMazeEnds.Invoke();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Unity-EscapeMaze
+Learning Unity: Escape Maze Game Prototype
+
+
+![alt text](https://raw.githubusercontent.com/shacharoz/Learning-Unity-Escape-Maze/master/Builds/escape-maze-logo.png)
+
+![alt text](https://raw.githubusercontent.com/shacharoz/Learning-Unity-Escape-Maze/master/Builds/escape-maze-top.png)
